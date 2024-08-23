5fe388ae6b9d7571a9663e87f8aba70cfa3f2bab
Rohit <rohitgupta22022003@gmail.com>
2024-08-23 19:08:09 +0530

    whitenoise

--- fitness_website/settings.py
+++ fitness_website/settings.py
@@ -47,6 +47,7 @@ AUTHENTICATION_BACKENDS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
