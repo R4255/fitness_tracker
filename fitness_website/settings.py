94c0b406314d9812d4193ab16d407cfeef0bedd1
Rohit <rohitgupta22022003@gmail.com>
2024-08-23 16:19:23 +0530

    changes for render

--- fitness_website/settings.py
+++ fitness_website/settings.py
@@ -88,12 +88,15 @@ WSGI_APPLICATION = "fitness_website.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+import dj_database_url
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -130,6 +133,10 @@ USE_I18N = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / 'fitness_app/static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
